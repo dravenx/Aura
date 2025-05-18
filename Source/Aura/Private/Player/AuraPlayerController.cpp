c8596274b5670516d9c2700ac6f0e880c78cc69e
les.ellis@haenir.com <les.ellis@haenir.com>
2025-05-18 12:36:33 +0100

    Added Input actions and input mapping context for aura character Created aura player controller to handle controls

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,5 @@
+// Copyright 2025 Les Ellis
+
+
+#include "Player/AuraPlayerController.h"
+
---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright 2025 Les Ellis
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
