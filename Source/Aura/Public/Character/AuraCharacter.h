5eed32c92d34236eca53844b0468f89ca6999301
les.ellis@haenir.com <les.ellis@haenir.com>
2025-05-18 11:10:41 +0100

    Adding new character classes for Aura and enemy

---
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -0,0 +1,17 @@
+// Copyright 2025 Les Ellis
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "AuraCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -0,0 +1,17 @@
+// Copyright 2025 Les Ellis
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "AuraEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraEnemy : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
